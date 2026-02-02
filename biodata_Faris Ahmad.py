219c37e89dd0538e6c61c09459abead71eb964f6
farisahmadmubaroq-cloud <farisahmadmubaroq@smkitihsanulfikri.sch.id>
2026-02-02 14:49:21 +0700

    Update biodata_Faris Ahmad.py

--- biodata_Faris Ahmad.py
+++ biodata_Faris Ahmad.py
@@ -3,7 +3,7 @@ print("Program Biodata Sederhana Dengan Python")
 print("")
 nama "Isi dengan Nama Kanu"
 hobi "Isi dengan Hobi kanu"
-alamat "Isi dengan alamat kamu"
+alamat "Isi dengan kamu"
 kota "Isi dengan Kota kamu"
 print("Nama Lengkap nama)
 print("Alamat
